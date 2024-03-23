5a51e052798dc22f81d3b30ba2c92960412b9924
williamstayo <williamstayo15@gmail.com>
2024-03-23 17:17:36 +0000

    implement clerk webhooks

--- src/lib/database/models/image.model.ts
+++ src/lib/database/models/image.model.ts
@@ -1,43 +1,43 @@
+import { Document, Schema, model, models } from "mongoose";
 
-
-import { Schema,models, model } from "mongoose";   // let's create our first mongoose model
-
-export interface Image extends Document {
-    title: string;
-    transformationType: string;
-    publicId: string;
-    secureUrl: string; // URL can be represented as a string for type purposes
-    width?: number;
-    height?: number;
-    config?: Record<string, any>; // 'Object' type in mongoose schema can be represented as Record<string, any> in TypeScript for a flexible key-value pair object
-    transformationUrl?: string;
-    aspectRatio?: string;
-    color?: string;
-    prompt?: string;
-    author: string; // Assuming the ObjectId is represented as a string in TypeScript. If you are working directly with MongoDB objects, you might use some specific ObjectId type provided by a library like mongoose
-    createdAt: Date;
-    updatedAt: Date;
+export interface IImage extends Document {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureURL: string; 
+  width?: number;
+  height?: number;
+  config?: object; 
+  transformationUrl?: string; 
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  }
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-const ImageSchema = new Schema({   // let's define the schema for our model
-    title: { type: String, required: true},
-    transformationType: {type: String, required: true},
-    publicId: { type: String, required: true},
-    secureUrl: {type: URL, required: true},
-    width: {type: Number},
-    height: {type: Number},
-    config: {type: Object},
-    transformationUrl: {type: String},
-    aspectRatio: {type: String},
-    color: {type: String},
-    prompt: {type: String},
-    author: {type: Schema.Types.ObjectId, ref:'User'}, 
-    createdAt: {type:Date, default: Date.now},
-    UpdatedAt: {type:Date, default: Date.now},
-
+const ImageSchema = new Schema({
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureURL: { type: String, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: String },
+  aspectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 });
 
-
-const Image = models?.Image || model('Image', ImageSchema); // let's turn our schema to a model for our app
+const Image = models?.Image || model('Image', ImageSchema);
 
 export default Image;
--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -2,35 +2,33 @@ import mongoose, {Mongoose} from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-// creating a database connection
-interface MongooseConnection{
-conn: Mongoose | null;
-promise: Promise<Mongoose> | null;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
 // let cache our mongodb connection
 let cached: MongooseConnection = (global as any).mongoose
 
-// check if we have a connection
 if(!cached) {
     cached = (global as any).mongoose = {
         conn: null, promise: null
     }
 }
 
-// if there is no connection let's create a new one 
-export const connectToDatabase =  async () => {
-    if(cached.conn) return cached.conn;
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
 
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-if(!MONGODB_URL) throw new Error('Missing M0NGODB_URL');
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
+    })
 
-cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
-    dbName: 'Editifyai', bufferCommands: false
-})
+  cached.conn = await cached.promise;
 
-cached.conn = await cached.promise
-
-return cached.conn;
+  return cached.conn;
 }
-
